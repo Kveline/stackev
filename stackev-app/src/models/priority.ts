fd41ae1714b926ebbd9e900430c8119e93f02543
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-01-30 11:36:17 +0700

    Priority Data Model

--- stackev-app/src/models/priority.ts
+++ stackev-app/src/models/priority.ts
@@ -1,3 +1,17 @@
-class Priority {}
+interface TechnologyAttribute {
+  Skalabilitas: number;
+  toolsSupport: number;
+  communitySize: number;
+  costLevel: number;
+  maturity: number;
+}
+
+class Priority {
+  priorityScore: TechnologyAttribute;
+
+  constructor(userPriority: TechnologyAttribute) {
+    this.priorityScore = userPriority;
+  }
+}
 
 export default Priority;
