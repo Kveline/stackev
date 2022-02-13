20568b188ef6930ab3b4262293e90abeab9cb939
yusufyp <yusuf.yudhistirazhy@gmail.com>
2022-02-13 13:17:59 +0700

    input maturity, cost level,  community size, tools support

--- stackev-app/src/App.tsx
+++ stackev-app/src/App.tsx
@@ -3,6 +3,7 @@ import { geneticAlgorithm } from "./helper/GeneticAlgorithm";
 import useInput from "./hooks/useInput";
 
 function App() {
+  // input skalabilitas
   const {
     value: enteredSkalabilitas,
     isValid: enteredSkalabilitasIsValid,
@@ -13,10 +14,60 @@ function App() {
     (value: any) => value.trim() !== "" && +value > -1 && +value < 11
   );
 
+  // input tool support
+  const {
+    value: enteredToolsSupport,
+    isValid: enteredToolsSupportIsValid,
+    hasError: toolsSupportInputHasError,
+    valueChangeHandler: toolsSupportChangedHandler,
+    inputBlurHandler: toolsSupportBlurHandler,
+  } = useInput(
+    (value: any) => value.trim() !== "" && +value > -1 && +value < 11
+  );
+
+  // input community size
+  const {
+    value: enteredCommunitySize,
+    isValid: enteredCommunitySizeIsValid,
+    hasError: communitySizeInputHasError,
+    valueChangeHandler: communitySizeChangedHandler,
+    inputBlurHandler: communitySizeBlurHandler,
+  } = useInput(
+    (value: any) => value.trim() !== "" && +value > -1 && +value < 11
+  );
+
+  // input community size
+  const {
+    value: enteredCostLevel,
+    isValid: enteredCostLevelIsValid,
+    hasError: costLevelInputHasError,
+    valueChangeHandler: costLevelChangedHandler,
+    inputBlurHandler: costLevelBlurHandler,
+  } = useInput(
+    (value: any) => value.trim() !== "" && +value > -1 && +value < 11
+  );
+
+  // input maturity
+  const {
+    value: enteredMaturity,
+    isValid: enteredMaturityIsValid,
+    hasError: maturityInputHasError,
+    valueChangeHandler: maturityChangedHandler,
+    inputBlurHandler: maturityBlurHandler,
+  } = useInput(
+    (value: any) => value.trim() !== "" && +value > -1 && +value < 11
+  );
+
   // checking all input is valid
   let formIsValid = false;
 
-  if (enteredSkalabilitasIsValid) {
+  if (
+    enteredSkalabilitasIsValid &&
+    enteredToolsSupportIsValid &&
+    enteredCommunitySizeIsValid &&
+    enteredCostLevelIsValid &&
+    enteredMaturityIsValid
+  ) {
     formIsValid = true;
   }
 
@@ -50,6 +101,98 @@ function App() {
             )}
           </div>
           {/* end of input skalabilitas */}
+
+          {/* input tool support */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Tools Support
+            </label>
+            <input
+              onChange={toolsSupportChangedHandler}
+              value={enteredToolsSupport}
+              onBlur={toolsSupportBlurHandler}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Tools support"
+            />
+            {toolsSupportInputHasError && (
+              <p className="text-rose-500 text-sm">Invalid input!</p>
+            )}
+          </div>
+          {/* end of input tool support */}
+
+          {/* input Community Size */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Community Size
+            </label>
+            <input
+              onChange={communitySizeChangedHandler}
+              value={enteredCommunitySize}
+              onBlur={communitySizeBlurHandler}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Community Size"
+            />
+            {communitySizeInputHasError && (
+              <p className="text-rose-500 text-sm">Invalid input!</p>
+            )}
+          </div>
+          {/* end of input Community Size */}
+
+          {/* input cost level */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Cost Level
+            </label>
+            <input
+              onChange={costLevelChangedHandler}
+              value={enteredCostLevel}
+              onBlur={costLevelBlurHandler}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Cost level"
+            />
+            {costLevelInputHasError && (
+              <p className="text-rose-500 text-sm">Invalid input!</p>
+            )}
+          </div>
+          {/* end of input cost level */}
+
+          {/* input Maturity */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Maturity
+            </label>
+            <input
+              onChange={maturityChangedHandler}
+              value={enteredMaturity}
+              onBlur={maturityBlurHandler}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Maturity"
+            />
+            {maturityInputHasError && (
+              <p className="text-rose-500 text-sm">Invalid input!</p>
+            )}
+          </div>
+          {/* end of input maturity */}
         </form>
       </div>
     </div>
